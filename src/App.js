7c4a96ccd735b277a618db7326459c2b3ede21d7
felishaww <felishamiranda1510@gmail.com>
2020-09-19 19:18:27 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,121 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Button from './components/Button';
+import Input from './components/Input';
+import ClearButton from './components/ClearButton';
+import Footer from './components/Footer';
 
-function App() {
-  return (
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      input: "",
+      prevNum: "",
+      currNum: "",
+      operator: ""
+    };
+  }
+
+  addToInput = val => {
+    this.setState({input: this.state.input + val})
+  };
+
+  addZeroToInput = val => {
+    if(this.state.input !== ""){
+      this.setState({ input: this.state.input +val});
+    }
+  };
+
+  clearInput = () => {
+    this.setState({ input: ""});
+  }
+;
+  tambah = () => {
+    this.setState({prevNum : this.state.input});
+    this.setState({input : ""});
+    this.setState({operator : "sum"});
+  };
+
+  kurang = () => {
+    this.setState({prevNum : this.state.input});
+    this.setState({input : ""});
+    this.setState({operator : "min"});
+  };
+
+  kali = () => {
+    this.setState({prevNum : this.state.input});
+    this.setState({input : ""});
+    this.setState({operator : "multiply"});
+  };
+
+  bagi = () => {
+    this.setState({prevNum : this.state.input});
+    this.setState({input : ""});
+    this.setState({operator : "divide"});
+  };
+
+  calculate = () => {
+    this.setState({currNum : this.state.input});
+    if(this.state.operator === "sum"){
+      this.setState({
+        input: parseInt(this.state.prevNum) + parseInt(this.state.currNum)
+      });
+    } else if (this.state.operator === "min"){
+      this.setState({
+        input: parseInt(this.state.prevNum) - parseInt(this.state.currNum)
+      });
+    } else if (this.state.operator === "multiply"){
+      this.setState({
+        input: parseInt(this.state.prevNum) * parseInt(this.state.currNum)
+      });
+    } else if (this.state.operator === "divide"){
+      this.setState({
+        input: parseInt(this.state.prevNum) / parseInt(this.state.currNum)
+      });
+    }
+  }
+
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calc-container">
+        <div className="input">
+          <Input>{this.state.input}</Input>
+        </div>
+        <div className="row">
+          <Button handleClick={this.addToInput}>7</Button>
+          <Button handleClick={this.addToInput}>8</Button>
+          <Button handleClick={this.addToInput}>9</Button>
+          <Button handleClick={this.bagi}>/</Button>
+        </div>
+        <div className="row">
+          <Button handleClick={this.addToInput}>4</Button>
+          <Button handleClick={this.addToInput}>5</Button>
+          <Button handleClick={this.addToInput}>6</Button>
+          <Button handleClick={this.kali}>*</Button>
+        </div>
+        <div className="row">
+          <Button handleClick={this.addToInput}>1</Button>
+          <Button handleClick={this.addToInput}>2</Button>
+          <Button handleClick={this.addToInput}>3</Button>
+          <Button handleClick={this.tambah}>+</Button>
+        </div>
+        <div className="row">
+          <Button> </Button>
+          <Button handleClick={this.addZeroToInput}>0</Button>
+          <Button handleClick={this.calculate}>=</Button>
+          <Button handleClick={this.kurang}>-</Button>
+        </div>
+        <div className="row">
+          <ClearButton handleClear={this.clearInput} />
+        </div>
+      </div>
+      <div className="footer">
+        <Footer />
+      </div>
     </div>
   );
+}
 }
 
 export default App;
